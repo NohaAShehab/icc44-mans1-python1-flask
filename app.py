b0bf895920f0900830b339c635f6ad7a22a9b8c0
Noha <nohashehab.iti@gmail.com>
2023-10-18 17:07:58 +0300

    playing with routes and return types

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 
 
 from flask import Flask
+from flask import request
 
 # create new flask application
 app=  Flask(__name__)  # use flask --app app run
@@ -10,6 +11,24 @@ app=  Flask(__name__)  # use flask --app app run
 @app.route("/")
 def hello_world():
     return "<h1 style='color:red'>Hello, World!</h1>"
+
+
+@app.route('/request')
+def request_info():
+    print(f"request is here --> {request}")
+    return  "<h1 style='color:green; text-align:center'>  This request </h1>"
+
+courses = ['django', 'flask', 'odoo', 'react', 'docker']
+@app.route('/courses')
+def get_courses():
+    return courses
+
+@app.route('/courses/<int:index>')
+def get_course(index):
+    try:
+        return courses[index]
+    except:
+        return "404"
 
 ## another option to the application
 if __name__=='__main__':
