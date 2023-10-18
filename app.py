6a903e7bc085c93802692290ba9e585e9cb6efdd
Noha <nohashehab.iti@gmail.com>
2023-10-18 17:16:06 +0300

    configrue add_url and page 404

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-
-
 from flask import Flask
 from flask import request
 
 # create new flask application
-app=  Flask(__name__)  # use flask --app app run
+app = Flask(__name__)  # use flask --app app run
 
 
-### define your first route
+# define your first route
 @app.route("/")
 def hello_world():
     return "<h1 style='color:red'>Hello, World!</h1>"
@@ -16,12 +14,16 @@ def hello_world():
 @app.route('/request')
 def request_info():
     print(f"request is here --> {request}")
-    return  "<h1 style='color:green; text-align:center'>  This request </h1>"
+    return "<h1 style='color:green; text-align:center'>  This request </h1>"
+
 
 courses = ['django', 'flask', 'odoo', 'react', 'docker']
+
+
 @app.route('/courses')
 def get_courses():
     return courses
+
 
 @app.route('/courses/<int:index>')
 def get_course(index):
@@ -30,8 +32,28 @@ def get_course(index):
     except:
         return "404"
 
+
+"adding url roles "
+
+
+def mynewurl():
+    return "<h1> Hello form my new url </h1>"
+
+# register function to new url
+app.add_url_rule('/newurl', view_func=mynewurl)
+
+
+"------------ creating new page for 404 not found error---------"
+@app.errorhandler(404)
+def page_not_found(error):
+    print(error)
+    return  (f"<h1 style='color:red; text-align:center'> "
+             f"Sorry Requested page Not Found !!!"
+             f"{error}"
+             f"</h1>")
+
 ## another option to the application
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
 
 # from terminal
